898d78925aab42cdb1055aa332c0881dfc6b62c7
dilipannztech <dilip@annztech.com>
2024-05-15 13:01:16 +0530

    changed to es2016

--- dist/bcryptFunctions.js
+++ dist/bcryptFunctions.js
@@ -1,59 +1,33 @@
 "use strict";
-var __awaiter =
-  (this && this.__awaiter) ||
-  function (thisArg, _arguments, P, generator) {
-    function adopt(value) {
-      return value instanceof P
-        ? value
-        : new P(function (resolve) {
-            resolve(value);
-          });
-    }
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
-      function fulfilled(value) {
-        try {
-          step(generator.next(value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function rejected(value) {
-        try {
-          step(generator["throw"](value));
-        } catch (e) {
-          reject(e);
-        }
-      }
-      function step(result) {
-        result.done
-          ? resolve(result.value)
-          : adopt(result.value).then(fulfilled, rejected);
-      }
-      step((generator = generator.apply(thisArg, _arguments || [])).next());
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.checkPassword = exports.hashPassword = void 0;
 const bcrypt_1 = __importDefault(require("bcrypt"));
 // Function to hash the password
 function hashPassword(password) {
-  return __awaiter(this, void 0, void 0, function* () {
-    const saltRounds = 10; // Number of salt rounds
-    const hashedPassword = yield bcrypt_1.default.hash(password, saltRounds);
-    return hashedPassword;
-  });
+    return __awaiter(this, void 0, void 0, function* () {
+        const saltRounds = 10; // Number of salt rounds
+        const hashedPassword = yield bcrypt_1.default.hash(password, saltRounds);
+        return hashedPassword;
+    });
 }
 exports.hashPassword = hashPassword;
 // Function to check if the password matches the hashed password
 function checkPassword(password, hashedPassword) {
-  return __awaiter(this, void 0, void 0, function* () {
-    const isMatch = yield bcrypt_1.default.compare(password, hashedPassword);
-    return isMatch;
-  });
+    return __awaiter(this, void 0, void 0, function* () {
+        const isMatch = yield bcrypt_1.default.compare(password, hashedPassword);
+        return isMatch;
+    });
 }
 exports.checkPassword = checkPassword;
