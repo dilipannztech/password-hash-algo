0983fc9e219119e19224b79892e4f5a757c312f1
dilipannztech <dilip@annztech.com>
2024-05-15 18:30:11 +0530

    empty

--- server.js
+++
@@ -1,6 +0,0 @@
-const { hashPassword, checkPassword } = require("./dist/bcryptFunctions");
-
-const passwors = hashPassword("dilip");
-const verifyPassword = checkPassword("dilip", passwors);
-
-console.log(passwors);
