9ae2fa7f8937e1f577e0ae7474fa8bf2bb8fa965
dilipannztech <dilip@annztech.com>
2024-05-15 18:29:48 +0530

    es6 update

--- dist/bcryptFunctions.js
+++ dist/bcryptFunctions.js
@@ -1,32 +1,24 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkPassword = exports.hashPassword = void 0;
-const bcrypt_1 = __importDefault(require("bcrypt"));
+import bcrypt from "bcrypt";
 // Constant for salt rounds
 const SALT_ROUNDS = 10;
 // Function to hash the password
-function hashPassword(password) {
+export function hashPassword(password) {
     try {
-        const salt = bcrypt_1.default.genSaltSync(SALT_ROUNDS);
-        const hashedPassword = bcrypt_1.default.hashSync(password, salt);
+        const salt = bcrypt.genSaltSync(SALT_ROUNDS);
+        const hashedPassword = bcrypt.hashSync(password, salt);
         return hashedPassword;
     }
     catch (error) {
         throw new Error(`Error hashing password: ${error}`);
     }
 }
-exports.hashPassword = hashPassword;
 // Function to check if the password matches the hashed password
-function checkPassword(password, hashedPassword) {
+export function checkPassword(password, hashedPassword) {
     try {
-        const isMatch = bcrypt_1.default.compareSync(password, hashedPassword);
+        const isMatch = bcrypt.compareSync(password, hashedPassword);
         return isMatch;
     }
     catch (error) {
         throw new Error(`Error checking password: ${error}`);
     }
 }
-exports.checkPassword = checkPassword;
--- dist/cryptoFunctions.js
+++ dist/cryptoFunctions.js
@@ -1,19 +1,14 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkPassword = exports.hashPassword = void 0;
-const crypto_1 = require("crypto");
+import { createHash } from "crypto";
 // Static salt value
 const SALT = 10;
 // Function to hash the password
-function hashPassword(password) {
-    const hash = (0, crypto_1.createHash)("sha256");
+export function hashPassword(password) {
+    const hash = createHash("sha256");
     hash.update(password + SALT);
     return hash.digest("hex");
 }
-exports.hashPassword = hashPassword;
 // Function to check if the password matches the hashed password
-function checkPassword(password, hashedPassword) {
+export function checkPassword(password, hashedPassword) {
     const hash = hashPassword(password);
     return hash === hashedPassword;
 }
-exports.checkPassword = checkPassword;
